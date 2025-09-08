2ba3ef76f1b7d34f9a48c529392d9047d92a40d7
Thomas Haslwanter <office@thaslwanter.at>
2025-09-08 22:10:47 +0200

    working at home

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -7,29 +7,6 @@
 -- and plugins.
 -----------------------------------------------------------
 --
--- local g = vim.g -- global variables
--- local map = vim.api.nvim_set_keymap
--- local default_opts = { noremap = true, silent = true }
-
--- g.python3_host_prog = "C:/Programs/WPy64-31220/python-3.12.2amd64/python.exe"
---
---
--- -- vim.api.nvim_create_autocmd("FileType", {
--- --   pattern = "python",
--- --   callback = function()
--- --     vim.api.nvim_buf_set_keymap(
--- --       0,
--- --       "n",
--- --       "<F9>",
--- --       "<cmd>!python " .. vim.fn.shellescape(vim.fn.expand("%"), 1) .. "<CR>",
--- --       { noremap = true, silent = true }
--- --     )
--- --   end,
--- -- })
--- --
--- -- Spelling
--- map('n', '<F9>', "<Cmd>!python test3.py<CR>", { noremap = true })-
---
 
 vim.keymap.set("n", "<F9>", function()
   vim.cmd("!python " .. vim.fn.shellescape(vim.fn.expand("%")))
--- lua/config/options.lua
+++ lua/config/options.lua
@@ -3,18 +3,4 @@
 -- Add any additional options here
 --
 
-local cmd = vim.cmd
-local g = vim.g
-
-vim.opt_local.relativenumber = false
-
----------------------------------------------------------
--- Python, execute code by pushing F9
----------------------------------------------------------
--- g.python3_host_prog = "C:/Programs/WPy64-31220/python-3.12.2amd64/python.exe"
---
--- cmd([[
---     autocmd FileType python nnoremap <buffer> <F9> :exec '!python' shellescape(@%, 1)<cr>
--- ]])
-
--- autocmd FileType python colorscheme nord
+vim.wo.relativenumber = false
